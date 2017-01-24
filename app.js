727539274df4ce39ba94249b16236cc48714e1a5
Toxicable <fabianwiles@live.com>
2017-01-25 08:25:54 +1300

    changed up mapping and contains replies

--- app.js
+++ app.js
@@ -24,16 +24,20 @@ const replyOptions = {
     method: 'POST'
 };
 
-const responses = {
-    gettingStarted: `- To get started with Angular, do [Quickstart](https://angular.io/docs/ts/latest/quickstart.html). After that, go through [Tour of Heroes (ToH)](https://angular.io/docs/ts/latest/tutorial/). Check the [Guide](https://angular.io/docs/ts/latest/guide/) for how to proceed after that. Good (often advanced) blogs are [Thoughtram](http://blog.thoughtram.io/exploring-angular-2/) and [Victor Savkin's blog](https://vsavkin.com/). A good starting point for a project is [angular-cli](https://github.com/angular/angular-cli)`,
-    notEnoughInfo: `- Please provide a brief description of what's going with the code. Pasting the error and saying "it's not working" doesn't help us help you. When did the problem start happening? Does it work if you remove/add some lines? Does it work in a different context? What have you tried so far?`,
-    createPlunker: `- It's very helpful to [create a plunker](http://plnkr.co/edit/tpl:AvJOMERrnz94ekVua0u5?p=catalogue) where you create a **minimal** reproduction of your problem, when possible. You're also likely to actually solve the problem yourself while doing so.`,
-    pleaseFormat: `- Please format your code. At the bottom-right of the chat's input box there is a \`M\` (Markdown) button which explains how to format snippets.`,
-    createGist: `- Please [create a gist](https://gist.github.com/) if your code is more than 8-10 lines long. It's difficult to keep track of the conversation in the chatroom when there is a huge block of code in the way. It's also difficult for us to look at your code this way.`,
-    notAngularQuestion: `- For **ng-cli** related questions, see [angular-cli on gitter](https://gitter.im/angular/angular-cli). For **ng-bootstrap**, [open a question on SO](http://stackoverflow.com/questions/tagged/ng-bootstrap).`,
-    angular3: `- There won't be Angular3. See more information on [Angular versions past version 2.x.x](http://angularjs.blogspot.co.nz/2016/12/ok-let-me-explain-its-going-to-be.html)`,
-    noStoredReply: `- I'm Sorry, I don't have a reply for that, i'm not very smart but you can help make better better by [contributing](https://github.com/Toxicable/angular-gitter-replybot)`
+const msgContainsReplies = {
+    gettingStarted: `To get started with Angular, do [Quickstart](https://angular.io/docs/ts/latest/quickstart.html). After that, go through [Tour of Heroes (ToH)](https://angular.io/docs/ts/latest/tutorial/). Check the [Guide](https://angular.io/docs/ts/latest/guide/) for how to proceed after that. Good (often advanced) blogs are [Thoughtram](http://blog.thoughtram.io/exploring-angular-2/) and [Victor Savkin's blog](https://vsavkin.com/). A good starting point for a project is [angular-cli](https://github.com/angular/angular-cli)`,
+    notEnoughInfo: `Please provide a brief description of what's going with the code. Pasting the error and saying "it's not working" doesn't help us help you. When did the problem start happening? Does it work if you remove/add some lines? Does it work in a different context? What have you tried so far?`,
+    createPlunker: `It's very helpful to [create a plunker](http://plnkr.co/edit/tpl:AvJOMERrnz94ekVua0u5?p=catalogue) where you create a **minimal** reproduction of your problem, when possible. You're also likely to actually solve the problem yourself while doing so.`,
+    pleaseFormat: `Please format your code. At the bottom-right of the chat's input box there is a \`M\` (Markdown) button which explains how to format snippets.`,
+    createGist: `Please [create a gist](https://gist.github.com/) if your code is more than 8-10 lines long. It's difficult to keep track of the conversation in the chatroom when there is a huge block of code in the way. It's also difficult for us to look at your code this way.`,
+    notAngularQuestion: `For **ng-cli** related questions, see [angular-cli on gitter](https://gitter.im/angular/angular-cli). For **ng-bootstrap**, [open a question on SO](http://stackoverflow.com/questions/tagged/ng-bootstrap).`,
+    angular3: `There won't be Angular3. See more information on [Angular versions past version 2.x.x](http://angularjs.blogspot.co.nz/2016/12/ok-let-me-explain-its-going-to-be.html)`,
 };
+const otherReplies = {
+    noStoredReply: `I'm Sorry, I don't have a reply for that, i'm not very smart but you can help make better better by [contributing](https://github.com/Toxicable/angular-gitter-replybot)`,
+
+}
+
 
 request(streamOptions)
     .on('error', handError)
@@ -72,43 +76,30 @@ function handError(error) {
 function getReply(text){
     text = text.toLowerCase(text);
     if(text.includes('angular3') || text.includes('angular 3')) {
-        return responses.angular3;
+        return msgContainsReplies.angular3;
     }
 
     if(text.startsWith(botKeyWord)){
-        if(text.includes('replies')){
-          return Object.keys(responses).reduce((a, b) => `${a}, ${b}`, `- Replies you can ask me for:`)
+        if(text.includes('help')){
+          return 'Replies you can ask me for: ' + Object.keys(msgContainsReplies)
+          .join(', ')
         }
 
-        if(text.includes('getting started')){
-            return responses.gettingStarted;
+        let reply = '';
+        Object.keys(msgContainsReplies)
+          .forEach(key => {
+            if(text.includes(key.toLowerCase())){
+                reply = msgContainsReplies[key];
+            }
+        })
+        if(!reply){
+
+        return otherReplies.noStoredReply;
+        }
+        else{
+          return reply;
         }
 
-        if(text.includes('not enough info')){
-            return responses.notEnoughInfo;
-        }
-
-        if(text.includes('create pl')){
-            return responses.createPlunker;
-        }
-
-        if(text.includes('create gist')){
-            return responses.createPlunker;
-        }
-
-        if(text.includes('format')){
-            return responses.pleaseFormat;
-        }
-
-        if(text.includes('not related')){
-            return responses.notAngularQuestion;
-        }
-
-        if(text.includes('not enough')){
-          return responses.notEnoughInfo;
-        }
-
-        return responses.noStoredReply;
     }
 
     return '';
