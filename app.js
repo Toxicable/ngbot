7dd4f8568600512786744e49f6cb6bcc4b1dc9a6
Toxicable <fabianwiles@live.com>
2017-01-26 00:11:46 +1300

    refactoring and changed reply maps

--- app.js
+++ app.js
@@ -25,18 +25,20 @@ const replyOptions = {
 };
 
 const msgContainsReplies = {
-  gettingStarted: `To get started with Angular, do [Quickstart](https://angular.io/docs/ts/latest/quickstart.html). After that, go through [Tour of Heroes (ToH)](https://angular.io/docs/ts/latest/tutorial/). Check the [Guide](https://angular.io/docs/ts/latest/guide/) for how to proceed after that. Good (often advanced) blogs are [Thoughtram](http://blog.thoughtram.io/exploring-angular-2/) and [Victor Savkin's blog](https://vsavkin.com/). A good starting point for a project is [angular-cli](https://github.com/angular/angular-cli)`,
-  notEnoughInfo: `Please provide a brief description of what's going with the code. Pasting the error and saying "it's not working" doesn't help us help you. When did the problem start happening? Does it work if you remove/add some lines? Does it work in a different context? What have you tried so far?`,
-  createPlunker: `It's very helpful to [create a plunker](http://plnkr.co/edit/tpl:AvJOMERrnz94ekVua0u5?p=catalogue) where you create a **minimal** reproduction of your problem, when possible. You're also likely to actually solve the problem yourself while doing so.`,
-  pleaseFormat: `Please format your code. At the bottom-right of the chat's input box there is a \`M\` (Markdown) button which explains how to format snippets.`,
-  createGist: `Please [create a gist](https://gist.github.com/) if your code is more than 8-10 lines long. It's difficult to keep track of the conversation in the chatroom when there is a huge block of code in the way. It's also difficult for us to look at your code this way.`,
-  notAngularQuestion: `For **ng-cli** related questions, see [angular-cli on gitter](https://gitter.im/angular/angular-cli). For **ng-bootstrap**, [open a question on SO](http://stackoverflow.com/questions/tagged/ng-bootstrap).`,
-  angular3: `There won't be Angular3. See more information on [Angular versions past version 2.x.x](http://angularjs.blogspot.co.nz/2016/12/ok-let-me-explain-its-going-to-be.html)`,
-  'route reuse': ` http://stackoverflow.com/questions/41280471/how-to-implement-routereusestrategy-shoulddetach-for-specific-routes-in-angular/41515648#41515648`
+  'getting started': `To get started with Angular, do [Quickstart](https://angular.io/docs/ts/latest/quickstart.html). After that, go through [Tour of Heroes (ToH)](https://angular.io/docs/ts/latest/tutorial/). Check the [Guide](https://angular.io/docs/ts/latest/guide/) for how to proceed after that. Good (often advanced) blogs are [Thoughtram](http://blog.thoughtram.io/exploring-angular-2/) and [Victor Savkin's blog](https://vsavkin.com/). A good starting point for a project is [angular-cli](https://github.com/angular/angular-cli)`,
+  'not enough info': `Please provide a brief description of what's going with the code. Pasting the error and saying "it's not working" doesn't help us help you. When did the problem start happening? Does it work if you remove/add some lines? Does it work in a different context? What have you tried so far?`,
+  'create plunker': `It's very helpful to [create a plunker](http://plnkr.co/edit/tpl:AvJOMERrnz94ekVua0u5?p=catalogue) where you create a **minimal** reproduction of your problem, when possible. You're also likely to actually solve the problem yourself while doing so.`,
+  'please format': `Please format your code. At the bottom-right of the chat's input box there is a \`M\` (Markdown) button which explains how to format snippets.`,
+  'createg gist': `Please [create a gist](https://gist.github.com/) if your code is more than 8-10 lines long. It's difficult to keep track of the conversation in the chatroom when there is a huge block of code in the way. It's also difficult for us to look at your code this way.`,
+  'not angularQuestion': `For **ng-cli** related questions, see [angular-cli on gitter](https://gitter.im/angular/angular-cli). For **ng-bootstrap**, [open a question on SO](http://stackoverflow.com/questions/tagged/ng-bootstrap).`,
+  'angular3': `There won't be Angular3. See more information on [Angular versions past version 2.x.x](http://angularjs.blogspot.co.nz/2016/12/ok-let-me-explain-its-going-to-be.html)`,
+  'route reuse': `For more information on the Route Reuse Stratergy [please see here](http://stackoverflow.com/questions/41280471/how-to-implement-routereusestrategy-shoulddetach-for-specific-routes-in-angular/41515648#41515648)`
+//trackBY
+
 };
 
 const otherReplies = {
-  noStoredReply: `I'm Sorry, I don't have a reply for that, i'm not very smart but you can help make better better by [contributing](https://github.com/Toxicable/angular-gitter-replybot)`,
+  noStoredReply: `I'm Sorry, I don't have a reply for that, i'm not very smart but you can help make me better better by [contributing](https://github.com/Toxicable/angular-gitter-replybot)`,
 
 }
 
@@ -51,7 +53,6 @@ request(streamOptions)
     }
 
     const msg = JSON.parse(msgString)
-    //if ((!msg.text.startsWith('ReplyBot:')) && (!msg.text.startsWith('DEBUG:'))) {
     if (msg.fromUser.id !== myId) {
       sendReply(msg);
       return;
@@ -69,7 +70,9 @@ function sendReply(msg) {
   const reply = Object.assign({}, replyOptions, { form: { text: replyText } });
   request(reply)
     .on('error', handError)
-    .on('response', response => console.log('reply sent'));
+    .on('response', response => {
+      console.log('reply sent')
+    });
 }
 
 function handError(error) {
@@ -88,30 +91,13 @@ function getReply(text) {
         .join(', ')
     }
 
-    let reply = '';
-    Object.keys(msgContainsReplies)
-      .forEach(key => {
-        let containsKey = key.toLowerCase()
+    const reply = Object.keys(msgContainsReplies)
+      .find(key =>  key.toLowerCase()
           .split()
           .every(part => text.includes(part))
+      )
 
-        if (containsKey) {
-          reply = msgContainsReplies[key];
-        }
-        //.map(part => text.includes(part))
-
-        // if(text.includes(key.toLowerCase())){
-        //     reply = msgContainsReplies[key];
-        // }
-      })
-    if (!reply) {
-
-      return otherReplies.noStoredReply;
-    }
-    else {
-      return reply;
-    }
-
+      return reply ? reply : otherReplies.noStoredReply;
   }
 
   return '';
