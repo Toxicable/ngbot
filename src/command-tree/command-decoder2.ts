4169411e1c63fa0a463d7184defeb31a34f50f9b
Toxicable <fabianwiles@live.com>
2017-04-08 23:13:08 -0600

    add travis and karma config

--- src/command-tree/command-decoder2.ts
+++ src/command-tree/command-decoder2.ts
@@ -1,29 +1,48 @@
 import { MessageBuilder } from './../util/message-builder';
 import { MessageModel } from './../angie/gitter.models';
+
 export class CommandDecoder2 {
   constructor(
+    //it must have a root node
     private root: CommandNode,
-  ) {
+  ) { }
 
-  }
+  //it needs a root but dosen't need extra children
   registerSubNodes(subNodes: CommandNode[]) {
     subNodes.forEach(node => this.root.children.push(node));
   }
 
   processMessage(message: MessageModel): MessageBuilder {
     const command = this.getMatch(message, this.root);
+    //its possible it might not pass back a function
     if (command instanceof Function) {
       return command(message);
     }
+    // so we jsut fail it silently and return an implicit undefined
   }
-  private getMatch(message: MessageModel, node: CommandNode): commandFn {
-    if (node.matcher && node.matcher.test(message.text)) {
-      return node.commandFn;
-    }
-    for (const subNode of node.children) {
-      return this.getMatch(message, subNode);
+
+  private getMatch(message: MessageModel, node: CommandNode): CommandFn {
+    //if it has a matcher than it's
+    //probably at the bottom of the tree
+    if (node.matcher) {
+      //return this fn only if we get a match
+      if (node.matcher.test(message.text)) {
+        return node.commandFn;
+      } else {
+        return null;
+      }
+    } else {
+      //only one of these should match
+      //so return the first match
+      for (const subNode of node.children) {
+        const childMatch = this.getMatch(message, subNode);
+        if (childMatch) {
+          return childMatch;
+        }
+      }
     }
   }
+
 }
 
 export class CommandNodeBuilder {
@@ -33,12 +52,12 @@ export class CommandNodeBuilder {
     matcher: null,
     children: [],
   };
-  withCommand(regex: RegExp, commandFn: commandFn) {
+  withCommand(regex: RegExp, commandFn: CommandFn) {
     this.node.matcher = regex;
     this.node.commandFn = commandFn;
     return this;
   }
-  withHelp(helpFn: helpFn) {
+  withHelp(helpFn: HelpFn) {
     this.node.helpFn = helpFn;
     return this;
   }
@@ -51,12 +70,12 @@ export class CommandNodeBuilder {
   }
 }
 
-export type commandFn = (message: MessageModel) => MessageBuilder;
-export type helpFn = () => MessageBuilder;
+export type CommandFn = (message: MessageModel) => MessageBuilder;
+export type HelpFn = () => MessageBuilder;
 
 export interface CommandNode {
-  helpFn: helpFn;
-  commandFn: commandFn;
+  helpFn: HelpFn;
+  commandFn: CommandFn;
   matcher: RegExp;
   children: CommandNode[];
 }
