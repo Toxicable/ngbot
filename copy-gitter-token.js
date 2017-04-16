f43a09b52338f583e4b065baddce708df75fc18c
Toxicable <fabianwiles@live.com>
2017-04-16 19:38:57 +1200

    fix(build): remove token from public view

--- copy-gitter-token.js
+++ copy-gitter-token.js
@@ -3,8 +3,6 @@ var fs = require('fs');
 var token =  process.env.TOKEN;
 var fileName = 'app.yaml';
 
-console.log(token);
-
 var data = fs.readFileSync(fileName, 'utf-8');
 data = data + '  TOKEN: ' + token;
 
