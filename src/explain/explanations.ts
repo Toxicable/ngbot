b9cd311beb143888d60f28f22b01078d810c8879
Fabian Wiles <fabianwiles@live.com>
2017-07-03 17:49:04 +1200

    pelase --> please

--- src/explain/explanations.ts
+++ src/explain/explanations.ts
@@ -16,6 +16,6 @@ export const explanations: Explaination[] = [
   { keys: ['global'], message: `Global doesn't exist in Angular's context, there is only scoping. For example if you have two modules (\`Module1\` and \`Module2\`), if you import an external Module that has a component: \`<spinner></spinner>\` it is only available in \`Module1\`, you will have to import it into \`Module2\` to access it there. See the [FAQ](https://angular.io/docs/ts/latest/cookbook/ngmodule-faq.html) for more info.`},
   { keys: ['cors'], message: `CORS is nothing to do with your client (being Angular). CORS is a server-side issue; there are two ways around it; you can either proxy your client server to the same origin as your API server or you can configure your API server to allow for requests from your clients origin`},
   { keys: ['async/await'], message: `Angular makes heavy use of Observable therefore the async/await pattern is generally not needed since you would have to convert your Observables to Promises which means you miss out of the cancellation/retry logic that Observables are so good at`},
-  { keys: ['conduct'], message: 'This room follows all rules outlined by [Angular\'s Code of Coduct](https://github.com/angular/code-of-conduct), pelase follow this or you may be banned'},
+  { keys: ['conduct'], message: 'This room follows all rules outlined by [Angular\'s Code of Coduct](https://github.com/angular/code-of-conduct), please follow this or you may be banned'},
   { keys: ['Angular1'], message: 'I think you meant AngularJS'}
 ] ;
